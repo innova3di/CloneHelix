ee378aacb1181c6964ff00e9217f03027c0283a5
Zhenislav <qa.oliinykyevhenii@gmail.com>
2021-11-29 21:45:33 +0300

    add project

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class EndLevel : MonoBehaviour {
+	
+	public Button nextLevel;
+	public string levelName;
+
+
+	void OnTriggerEnter(Collider other)
+		{
+		
+		    if(other.gameObject.name == "Player")
+		{
+			nextLevel.gameObject.SetActive (true);
+
+
+			//SceneManager.LoadScene (levelName);
+		}
+	}
+}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+	
+	public Button RestartButton;
+	public Button Quit;
+	public Text textscore;
+	public Text bestScore;
+	private int score;
+	//public AudioSource ;
+	public AudioClip jumpSound;
+	public AudioClip destSound;
+
+
+	void Start (){
+
+		Time.timeScale = 1.5f;
+		score = 0;
+		bestScore.text = "Best: " + PlayerPrefs.GetFloat ("Best");
+
+	}
+	
+	// ball when Toche redBar
+	void OnTriggerEnter(Collider other) 
+	{
+		if(other.gameObject.name == "Enmy")
+		{
+			RestartButton.gameObject.SetActive (true);
+			Quit.gameObject.SetActive (true);
+			Time.timeScale = 0f;
+
+		}
+
+		if(other.gameObject.name == "ScoreBar") 
+		{
+			score = score + 1;
+			if(PlayerPrefs.GetFloat ("Best") < score)
+			   PlayerPrefs.SetFloat ("Best", score);
+			
+			textscore.text = "" + score.ToString();
+			other.gameObject.SetActive (false);
+		}
+	}
+
+	void OnTriggerExit(Collider other) 
+	{
+		if(other.gameObject.name == "Circle")
+		{
+			HealthBar.health -= 1f;
+			AudioSource audio = GetComponent<AudioSource> ();
+			audio.PlayOneShot (destSound);
+			other.gameObject.SetActive (false);
+
+		}
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.collider.name=="Bar")
+		{
+			AudioSource audio = GetComponent<AudioSource> ();
+			audio.PlayOneShot (jumpSound);
+		}
+	}
+}
